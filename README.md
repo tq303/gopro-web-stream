499388b3cdb8f06574d6989cf1132e3b951ccd84
tq303 <contactoliverwhite@gmail.com>
2016-11-17 21:02:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GoPro Stream
+
+## Builds
+
+### Front-End
+
+#### Production
+```
+npm run build
+```
+
+#### Dev
+```
+npm run build:dev
+```
+
+### Serve
+```
+node .
+```
+_Default port `8000`_
---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const path    = require('path');
+const app     = express();
+const request = require('request');
+
+const PASSWORD = 'peachtree';
+const AUTH_GOPRO_REQUEST = `http://10.5.5.9/camera/PV?t=${PASSWORD}&p=%02`;
+const GOPRO_VIDEO_STREAM = 'http://10.5.5.9:8080/live/amba.m3u8';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let port = 8000;
+
+if (typeof process.env.PORT !== 'undefined') {
+  port = parseInt(process.env.PORT, 10);
+}
+
+app.listen(port);
+
+app.get('/stream', (req, res)=> {
+
+	request.get(AUTH_GOPRO_REQUEST, (err, response)=> {
+
+		request.get(GOPRO_VIDEO_STREAM)
+			.on('response', (response)=> {
+				console.log(response.statusCode);
+			})
+			.on('error', (err)=> {
+				console.log(err);
+			})
+			.pipe(res);
+	});
+});
+
+console.log('web-site-template on http://0.0.0.0:%d', port);
